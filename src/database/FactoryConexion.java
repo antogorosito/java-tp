1b67bc303e887bde17850b6cf67da0b95c4f8387
Sabrina Velazquez <velazquezsabrina@hotmail.com>
2018-11-16 14:22:05 -0300

    jre

--- src/database/FactoryConexion.java
+++ src/database/FactoryConexion.java
@@ -4,8 +4,8 @@ import java.sql.*;
 public class FactoryConexion {
 	
 	private String dbDriver = "com.mysql.cj.jdbc.Driver";
-//private String host ="localhost";
-	private String host = "node24155-java2018.jelastic.saveincloud.net";
+    private String host ="localhost";
+	//private String host = "node24155-java2018.jelastic.saveincloud.net";
 	private String port = "3306";
 	private String user = "javatp";
 	private String pass = "java2018";
@@ -40,12 +40,12 @@ public class FactoryConexion {
 		
 		if(conn==null || conn.isClosed()){
 		
-		//conn = DriverManager.getConnection("jdbc:"+dbType+"://"+host+":"+port+"/"+db+"?&useSSL=false&serverTimezone=America/Argentina/Buenos_Aires",user,pass);
+		conn = DriverManager.getConnection("jdbc:"+dbType+"://"+host+":"+port+"/"+db+"?&useSSL=false&serverTimezone=America/Argentina/Buenos_Aires",user,pass);
 			
-			conn = DriverManager.getConnection("jdbc:"+dbType+"://"+host+"/"+db+"?&useSSL=false&serverTimezone=America/Argentina/Buenos_Aires",user,pass);
+			//conn = DriverManager.getConnection("jdbc:"+dbType+"://"+host+"/"+db+"?&useSSL=false&serverTimezone=America/Argentina/Buenos_Aires",user,pass);
 	
 		}
-		return conn;
+		 return conn;
 	}
 	
 	public void releaseConn(){
